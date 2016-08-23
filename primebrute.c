e48b3733cafe60c494d833d8f271d6a163eeafc8
robgraves <matthewpage2003@yahoo.com>
2016-08-23 19:09:25 -0400

    working on primebrute.c

--- primebrute.c
+++ primebrute.c
@@ -5,7 +5,7 @@
 * Discrete Structures
 * 08/23/2016
 *
-* primebrute.c -    program to use a brute force to
+* primebrute.c -    program to use brute force to
 *                   calculate prime numbers.
 *
 **************************************************/
@@ -16,41 +16,53 @@
 
 int main(int argc, char **argv)
 {
+    //struct for time management
 	struct timeval time_start; // starting time
     struct timeval time_end;   // ending time
 
+    //initialization of variables
 	int max 	= 0; 
-	int i 		= 2;
-	float current = 0.0;
+	int i 		= 2;  //outer loop counter
+    int j       = 1;  //inner loop counter
+    int primal  = 0;  //"is prime" flag, 0 for no, 1 for yes
 
-	float test = 0.0; 
-
+    //convert command line argument from ASCII to integer
 	max = atoi(argv[1]);
 
+    //start timer
 	gettimeofday(&time_start, 0);
 
-	test = (max / 3.0);	
-
-	printf("%f", test);
-	
-	/*
+    //main loop
 	while (i < max)
 	{
-		current = (max % i);
+        //printf("outer iteration: %d\n", i);
+        while (j < max) 
+        {
+            //printf("inner iteration: %d\n", j);
+            if ((i % j) != 0)
+            {    
+                primal = 1;
+            }
+            j++;
+        }
+        j = 1;
 
-		if (current == 0)
+		if (primal == 1)
 		{
 			printf("%d ", i);
 		}  
+        primal = 0;
 		i++;  	
 
 	}
-	*/
-
-	printf("\n");
 	
+    //neatness...to make the children happy
+	printf("\n");
+
+	//end timer
 	gettimeofday(&time_end, 0);
 
+    //display runtime results
 	fprintf(stderr, "%10.6lf\n", time_end.tv_sec - time_start.tv_sec + ((time_end.tv_usec - time_start.tv_usec) / 1000000.0));	
 
 
