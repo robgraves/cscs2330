edadce579695bd872a1f908f1aa2d59e127d1f52
Matthew Page <me@matthewjpage.com>
2016-08-26 22:32:24 -0400

    added matrix program from class today

--- matrixadd.c
+++ matrixadd.c
@@ -5,24 +5,47 @@ typedef signed char sc;
 
 int main(int argc, char **argv)
 {
-    FILE *in;
-    FILE *out;
+	FILE  *in;
+	FILE  *out;
 
-    sc **matrixA;
-    sc **matrixB;
-    sc **matrixC;
+	sc   **matrixA;
+	sc   **matrixB;
+	sc   **matrixC;
 
-    int x;
-    int y;
+	sc    x;
+	sc    y;
+	sc    r1, r2;
+	sc    c1, c2;
 
-    if (argc < 3)
-    {
-            fprintf(stderr, "Error,: must provide at least 2 arguments\n");
-            fprintf(stderr, "Usage:\n");
-            fprintf(stderr, "\t%s matrix1file matrix2file [outputmatrixfile]\n", *(argv+0));
+	if (argc <  3)
+	{
+		fprintf(stderr, "Error: must provide at least 2 arguments\n");
+		fprintf(stderr, "Usage:\n");
+		fprintf(stderr, "\t%s matrix1file matrix2file [outputmatrixfile]\n", *(argv+0));
+		exit(1);
+	}
 
-            exit(1);
-    }
+	in = fopen(*(argv+1), "r");
+	if (in == NULL)
+	{
+		fprintf(stderr, "Error: Couldn't open '%s' for read!\n", *(argv+1));
+		exit(1);
+	}
 
-    return (0);
+	fscanf(in, "%hhd", &r1);
+	fscanf(in, "%hhd", &c1);
+
+	matrixA = (sc **) calloc(r1, sizeof(sc *));
+	for (x = 0; x < r1; x++)
+	{
+		*(matrixA+x) = (sc *) calloc(c1, sizeof(sc));
+		for (y = 0; y < c1; y++)
+		{
+			fscanf(in, "%hhd", *(*(matrixA+y)+x));
+		}
+	}
+
+	fclose(in);
+
+	return(0);
 }
